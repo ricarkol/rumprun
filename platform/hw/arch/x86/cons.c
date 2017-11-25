ee81d96d90122ef7a5d59fe829ae60e65e81ac32
Ricardo Koller <kollerr@us.ibm.com>
2017-11-24 19:23:57 -0500

    solo5: moved console solo5 implementation back to solo5

--- platform/hw/arch/x86/cons.c
+++ platform/hw/arch/x86/cons.c
@@ -30,79 +30,17 @@
 #include <arch/x86/hypervisor.h>
 
 #include <bmk-core/printf.h>
+#include <bmk-core/solo5.h>
 
 char bios_com1_base;
 char bios_crtc_base;
-
-/*
- * Filled in by locore from BIOS data area.
- */
-#define UKVM_HYPERCALL_PIO_BASE 0x500
-
-#define UKVM_GUEST_PTR(T) T
-
-
-/*
- * On x86, 32-bit PIO is used as the hypercall mechanism. This only supports
- * sending 32-bit pointers; raise an assertion if a bigger pointer is used.
- *
- * On x86 the compiler-only memory barrier ("memory" clobber) is sufficient
- * across the hypercall boundary.
- */
-static inline void ukvm_do_hypercall(int n, volatile void *arg)
-{
-#    ifdef assert
-    assert(((uint64_t)arg <= UINT32_MAX));
-#    endif
-    __asm__ __volatile__("outl %0, %1"
-            :
-            : "a" ((uint32_t)((uint64_t)arg)),
-              "d" ((uint16_t)(UKVM_HYPERCALL_PIO_BASE + n))
-            : "memory");
-}
-
-enum ukvm_hypercall {
-    /* UKVM_HYPERCALL_RESERVED=0 */
-    UKVM_HYPERCALL_WALLTIME=1,
-    UKVM_HYPERCALL_PUTS,
-    UKVM_HYPERCALL_POLL,
-    UKVM_HYPERCALL_BLKINFO,
-    UKVM_HYPERCALL_BLKWRITE,
-    UKVM_HYPERCALL_BLKREAD,
-    UKVM_HYPERCALL_NETINFO,
-    UKVM_HYPERCALL_NETWRITE,
-    UKVM_HYPERCALL_NETREAD,
-    UKVM_HYPERCALL_HALT,
-    UKVM_HYPERCALL_MAX
-};
-
-
-/* UKVM_HYPERCALL_PUTS */
-struct ukvm_puts {
-    /* IN */
-    UKVM_GUEST_PTR(const char *) data;
-    unsigned long len;
-};
-
-
-static int platform_puts(const char *buf, int n)
-{
-    struct ukvm_puts str;
-
-    str.data = (char *)buf;
-    str.len = n;
-
-    ukvm_do_hypercall(UKVM_HYPERCALL_PUTS, &str);
-
-    return str.len;
-}
 
 
 void
 cons_putc(int _c)
 {
 	char c = _c;
-	platform_puts(&c, 1);
+	solo5_console_write(&c, 1);
 }
 
 void
@@ -110,8 +48,6 @@ cons_init(void)
 {
 	bmk_printf_init(cons_putc, NULL);
 }
-
-
 
 static unsigned long strlen(const char * str)
 {
@@ -123,6 +59,6 @@ static unsigned long strlen(const char * str)
 void
 cons_puts(const char *s)
 {
-	platform_puts(s, strlen(s));
+	solo5_console_write(s, strlen(s));
 
 }
